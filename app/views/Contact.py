75204ad37d73cb887b8f77110cc905eafec9da36
Raphael Oliveira Cunha <47646405+raphaelarcanjo@users.noreply.github.com>
2022-05-31 17:36:20 -0300

    About view and layout fixings

---
+++ app/views/Contact.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+class Contact:
+    def index(self, request):
+        data = {
+            'title': 'Contact'
+        }
+
+        return render(request, 'contact.html', data)
--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('app.routes.home')),
     path('about/', include('app.routes.about')),
+    path('contact/', include('app.routes.contact')),
 ]
